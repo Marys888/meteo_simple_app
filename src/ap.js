c0798b718d305d69c637c3263485e3cd1ecac750
Maria Sievierova <129101728+Marys888@users.noreply.github.com>
2024-02-11 14:40:14 +0200

    added API  weather data to the project

--- src/ap.js
+++ src/ap.js
@@ -1,9 +1,28 @@
-function enterCity(event) {
-event.preventDefault();
-const inputElemnt = document.querySelector("#form__input");
-const cityElement = document.querySelector("#city");
-cityElement.innerHTML = inputElemnt.value;
-};
+function updateWeatherInfo(response) {
+    
+    let cityElement = document.querySelector("#city");
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity (city){
+    let apiKey = "a34t41e6f3486dfcf9dba23606b154co";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(updateWeatherInfo);
+}
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let inputElement = document.querySelector("#form__input");
+    
+    
+    searchCity(inputElement.value);
+}
 
 const form = document.querySelector("#form");
-form.addEventListener("submit", enterCity);
+form.addEventListener("submit", handleSearchSubmit);
+
+searchCity("kyiv");
